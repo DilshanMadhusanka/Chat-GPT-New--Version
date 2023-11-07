bf6459629b607526dc75d2c1a97245c11e518e1b
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-07 21:08:50 +0530

    set the file structure

---
+++ src/componetns/Navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='text-white '>
+      <h1 className='text-2xl text-[#00df9a]'>hello</h1>
+    </div>
+  )
+}
