5b79c97c43edfff7f91503e920ca8eddd72bd192
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-08 22:52:34 +0530

    update the navbar code

--- src/componetns/Navbar.jsx
+++ src/componetns/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
-import { useState } from "react";
 
 export default function Navbar() {
   const [Nav, setNav] = useState(false);
@@ -12,24 +12,20 @@ export default function Navbar() {
   return (
     <div className="flex items-center justify-between h-24 text-white max-w-[1240px] px-4 mx-auto">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">React</h1>
-      <ul className="flex hidden">
-        <li className="p-4">Home</li>
-        <li className="p-4">Company</li>
-        <li className="p-4">Resources</li>
-        <li className="p-4">About</li>
-        <li className="p-4">Contact</li>
-      </ul>
-      <div onClick={handleNav}>
-        {!Nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+      <div className="hidden md:flex">
+        <ul className="flex">
+          <li className="p-4">Home</li>
+          <li className="p-4">Company</li>
+          <li className="p-4">Resources</li>
+          <li className="p-4">About</li>
+          <li className="p-4">Contact</li>
+        </ul>
+      </div>
+      <div onClick={handleNav} className="md:hidden">
+        {Nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
-      <div
-        className={
-          !Nav
-            ? "fixed top-0 left-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
-        }
-      >
+      <div className={Nav ? "fixed top-0 left-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">React</h1>
         <ul className="p-4 uppercase">
           <li className="p-4 border-b border-gray-600">Home</li>
@@ -42,3 +38,4 @@ export default function Navbar() {
     </div>
   );
 }
+
