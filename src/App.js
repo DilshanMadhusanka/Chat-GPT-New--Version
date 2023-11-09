ad19370633fbf5a5bd20166ac5b46e1219eef9f2
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-09 21:58:25 +0530

    Hero Section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./componetns/Navbar";
+import Hero from './componetns/Hero'
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero />
     </div>
   );
 }
