24e888e84f040f16ef1b2e3f6f8af2158c35e406
DilshanMadhusanka <139458721+DilshanMadhusanka@users.noreply.github.com>
2023-11-11 23:40:41 +0530

    Get Started Button

--- src/componetns/Navbar.jsx
+++ src/componetns/Navbar.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
@@ -19,13 +18,25 @@ export default function Navbar() {
           <li className="p-4">Resources</li>
           <li className="p-4">About</li>
           <li className="p-4">Contact</li>
+          <li className="py-1 text-black  px-14 bg-slate-50">
+            <a href="#">Get_Started</a>
+          </li>
+
+
+
         </ul>
       </div>
       <div onClick={handleNav} className="md:hidden">
         {Nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
       </div>
 
-      <div className={Nav ? "fixed top-0 left-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500" : "fixed left-[-100%]"}>
+      <div
+        className={
+          Nav
+            ? "fixed top-0 left-0 w-[60%] h-full border-r border-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "fixed left-[-100%]"
+        }
+      >
         <h1 className="w-full text-3xl font-bold text-[#00df9a]">React</h1>
         <ul className="p-4 uppercase">
           <li className="p-4 border-b border-gray-600">Home</li>
@@ -33,9 +44,11 @@ export default function Navbar() {
           <li className="p-4 border-b border-gray-600">Resources</li>
           <li className="p-4 border-b border-gray-600">About</li>
           <li className="p-4">Contact</li>
+          <li className="p-4 text-black rounded-sm bg-slate-50">
+            <a href="#">Get Stated</a>
+          </li>
         </ul>
       </div>
     </div>
   );
 }
-
